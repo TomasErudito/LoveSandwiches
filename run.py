f55b2b5bb102dba481d9db7ed0314f543c9c7b42
TomasErudito <tomas@oppossite.com>
2022-03-30 19:33:45 +0000

    data request loop

--- run.py
+++ run.py
@@ -17,15 +17,20 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market")
-    print("Data should be six numbers, sparated by commas")
-    print("Example: 1,2,3,4,5,6\n")
+    while True:
+        print("Please enter sales data from the last market")
+        print("Data should be six numbers, sparated by commas")
+        print("Example: 1,2,3,4,5,6\n")
 
-    data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:")
 
-    sales_data = data_str.split(",")
+        sales_data = data_str.split(",")
 
-    validate_data(sales_data)
+        if validate_data(sales_data):
+            print("data valid")
+            break
+
+        
 
 
 def validate_data(values):
@@ -42,6 +47,8 @@ def validate_data(values):
     except ValueError as e:
 
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-get_sales_data()
+data = get_sales_data()
